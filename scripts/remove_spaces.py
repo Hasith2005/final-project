3aa0f159ba3fefd513028c5dfb3400d6af71e8e9
hasith <hasith2005@gmail.com>
2026-03-24 23:58:08 +0000

    tested with units supplied..

--- scripts/remove_spaces.py
+++ scripts/remove_spaces.py
@@ -1,10 +1,16 @@
 import os
+import argparse
 
-DIR = "/home/hasi/projects/Feynman_with_units/Feynman_with_units"
-def remove_spaces():
-    for file in os.listdir(DIR):
+# DIR = "/home/hasi/projects/Feynman_with_units/Feynman_with_units"
+def remove_spaces(dir):
+    if not os.path.isdir(dir):
+        print(f"Error: The directory '{dir}' does not exist.")
+        return
+    print(f"scanning {dir}")
+    
+    for file in os.listdir(dir):
         fname = os.fsdecode(file)
-        fpath=os.path.join(DIR,fname)
+        fpath=os.path.join(dir,fname)
         print(f"editing {fname}")
         with open(fpath,"r") as f:
             lines=f.readlines()
@@ -15,5 +21,14 @@ def remove_spaces():
     
 
 if __name__=='__main__':
-    remove_spaces()
+    parser = argparse.ArgumentParser(
+        description="strip trailing spaces/tabs from all lines in a dataset's files."
+    )
+    parser.add_argument(
+        "directory", 
+        type=str, 
+        help="Path to the target directory containing the files"
+    )
+    args = parser.parse_args()
+    remove_spaces(args.directory)
 
